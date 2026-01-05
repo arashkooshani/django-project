84180b3c0691945834ba4950291791357981a8c4
arash <arash.kooshani@gmail.com>
2026-01-05 16:20:14 +0330

    added alot of things

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from .views import blog_view,blog_detail_view
 app_name = 'blog'
 urlpatterns = [
     path('',blog_view,name='blog_index'),
-    path('sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssblog-detail', blog_detail_view, name='blog_detail'),
+    path('<int:pid>',blog_detail_view,name='blog_detail')
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+
+from blog.models import Post
+
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts=Post.objects.filter(status=True)
+    context={'posts':posts}
+    return render(request, 'blog/blog-home.html',context)
 
-def blog_detail_view(request):
-    return render(request, 'blog/blog-single.html')
+def blog_detail_view(request,pid):
+    post=get_object_or_404(Post,pk=pid,status=True)
+    context={'post':post}
+    return render(request, 'blog/blog-single.html',context)
